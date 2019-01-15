3e1f908e886ad52f62e0b79654a4d2b3e683f158
Nick Neiert <nickster7421@gmail.com>
2019-01-15 15:29:29 -0600

    added header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Header extends Component{
+    render(){
+        return(
+            <div class ="header-tag">
+                <h1>Famous People</h1>
+            </div>
+        );
+    }
+}
+
+export default Header
